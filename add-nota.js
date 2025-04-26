6dfdc68a79c3ebe4a4f645d4cbbecdb0a3a99c91
Eryck Assis <eng.assis.dev@gmail.com>
2025-04-25 22:19:54 -0300

    Remove duplicate code and streamline average calculation in add-nota.js

--- add-nota.js
+++ add-nota.js
@@ -5,25 +5,3 @@ notas.push(7);
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(`A média é ${media}`);
-("");
-
-const notas = [10, 6, 8];
-
-notas.push(7);
-
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
-
-console.log(`A média é ${media}`);
-("");
-
-const notas = [10, 6, 8];
-notas.push(7);
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
-console.log(`A média é ${media}`);
-("");
-
-const notas = [10, 6, 8];
-notas.push(7);
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
-console.log(`A média é ${media}`);
-("");
