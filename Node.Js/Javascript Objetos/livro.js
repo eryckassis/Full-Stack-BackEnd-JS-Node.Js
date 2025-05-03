2182a07d0072294e409df8ee6eb584ebc0baf1d7
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-02 22:46:43 -0300

    Add initial book object and details logging

---
+++ Node.Js/Javascript Objetos/livro.js
@@ -0,0 +1,20 @@
+const objLivro = {
+  titulo: "Pirinha Bubu & Dudu",
+  autor: "Pirinha",
+  genero: "Aventura",
+  anoPublicado: "2020",
+};
+
+console.log(`Detalhes do Livro:
+    Titulo: ${objLivro.titulo}
+    Autor: ${objLivro.autor}
+    Genero: ${objLivro.genero}
+    Ano de publicação: ${objLivro.anoPublicado}`);
+
+//
+
+const anoAtual = new Date().getFullYear();
+
+const livro = {
+  titulo: "Novo Livro",
+};
