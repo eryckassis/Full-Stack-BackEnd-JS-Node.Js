cd9913db8f71cd0e80822336863b31cff9dc1ab8
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-05 19:47:03 -0300

    Add estudante and evento objects with address and birthday details

---
+++ Node.Js/Javascript Objetos/dados-valores.js
@@ -0,0 +1,21 @@
+const estudante = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312312",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["55119999998", "551199999994"],
+};
+
+estudante.endereco = {
+  rua: "Rua Josefa Maluca",
+  numero: "45",
+  complemento: "apto 43",
+};
+
+console.log(estudante.endereco.rua);
+
+const evento = {
+  nome: "Aniversario",
+  data: new Date(2000, 5, 15),
+};
