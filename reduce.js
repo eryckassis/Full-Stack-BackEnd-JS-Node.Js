6234ce67c85aae9fbafda362c018222457e9e8d3
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-06 23:19:40 -0300

    Refactor reduce.js to uncomment and restore original functionality for media calculation and logging

--- reduce.js
+++ reduce.js
@@ -1,19 +1,19 @@
-// const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
-// const salaJava = [6, 5, 8, 9, 5, 6];
-// const salaPython = [7, 3.5, 8, 9.5];
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
 
-// function calculaMedia(listaDeNotas) {
-//   const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
-//     return acumulador + nota;
-//   }, 0);
+function calculaMedia(listaDeNotas) {
+  const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
+    return acumulador + nota;
+  }, 0);
 
-//   const medua = somaDasNotas / listaDeNotas.length;
-//   return medua;
-// }
+  const medua = somaDasNotas / listaDeNotas.length;
+  return medua;
+}
 
-// console.log(calculaMedia(salaJS));
-// console.log(calculaMedia(salaJava));
-// console.log(calculaMedia(salaPython));
+console.log(calculaMedia(salaJS));
+console.log(calculaMedia(salaJava));
+console.log(calculaMedia(salaPython));
 
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
